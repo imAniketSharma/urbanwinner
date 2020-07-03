523d438841225260ab79588dea6a7d07b01277d6
Aniket Sharma <aniketsharma552@gmail.com>
2020-07-03 22:20:09 +0530

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -4,8 +4,8 @@ from email.mime.text import MIMEText
 def send_mail(name, email, phone, message):
     port = 2525
     smtp_server = 'smtp.mailtrap.io'
-    login = '358124e4561b2c'
-    password = '5b7f1f3d64c62d'
+    login = '*************'
+    password = '*************'
     message = f"<h3>New Contact message submission</h3><ul><li>Name: {name}</li><li>Email: {email}</li><li>Phone: {phone}</li><li>Message: {message}</li></ul>"
 
     sender_email = 'email1@example.com'
